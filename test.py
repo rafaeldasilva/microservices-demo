bbf5e191b34f0c22fb31b431d7ca1f9c3471a148
Rafael da Silva <rs.rafaeldasilva@gmail.com>
2024-01-19 17:42:54 -0300

    test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+# Criar um função que some dois numeros
+# e retorne o resultado
+
+def soma(a, b):
+    return a + b
+
+print(soma(10, 20))
+
+# Criar uma função que subtraia dois numeros
+# e retorne o resultado
+
+def subtracao(a, b):
+    return a - b
+
+print(subtracao(20, 10))
+
